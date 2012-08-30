680b4dad7e2ad5a3157fc322c6d62cb5e282b188
Matt Di Pasquale <liveloveprosper@gmail.com>
2012-08-30 07:19:57 -0500

    Add NSNetServiceBrowser.

--- AirChat/UsersTableViewController.h
+++ AirChat/UsersTableViewController.h
@@ -1,6 +1,8 @@
 #import <UIKit/UIKit.h>
 
-@interface UsersTableViewController : UITableViewController
+@interface UsersTableViewController : UITableViewController <NSNetServiceBrowserDelegate>
+
+@property (strong, nonatomic) NSMutableArray *services;
 
 @property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
 
--- AirChat/UsersTableViewController.m
+++ AirChat/UsersTableViewController.m
@@ -2,17 +2,32 @@
 
 @implementation UsersTableViewController
 
+#pragma mark - UITableViewController
+
+- (id)initWithStyle:(UITableViewStyle)style {
+    if (self = [super initWithStyle:style]) {
+        self.services = [NSMutableArray array];
+    }
+    return self;
+}
+
+#pragma mark - UIViewController
+
+- (void)viewWillAppear:(BOOL)animated {
+    [self searchForNetworkServices];
+}
+
 //#pragma mark - UITableViewDelegate
 //
 //- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-//     MessagesViewController *messagesViewController = [[MessagesViewController alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+//     MessagesViewController *messagesViewController = [[MessagesViewController alloc] initWithNibName:nil bundle:nil];
 //     [self.navigationController pushViewController:messagesViewController animated:YES];
 //}
 
 #pragma mark - UITableViewDataSource
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 1;
+    return [_services count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -22,8 +37,26 @@
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
         cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
     }
-    cell.textLabel.text = @"Matt Di Pasquale";
+    cell.textLabel.text = ((NSNetService *)[_services objectAtIndex:indexPath.row]).name;
     return cell;
+}
+
+#pragma mark - NSNetServiceBrowserDelegate
+
+- (void)searchForNetworkServices {
+    NSNetServiceBrowser *netServiceBrowser = [[NSNetServiceBrowser alloc] init];
+	netServiceBrowser.delegate = self;
+	[netServiceBrowser searchForServicesOfType:@"_airchat._tcp." inDomain:@""];
+}
+
+- (void)netServiceBrowser:(NSNetServiceBrowser *)netServiceBrowser didFindService:(NSNetService *)netService moreComing:(BOOL)moreServicesComing {
+    [_services addObject:netService];
+    NSLog(@"didFindService: %@", netService);
+}
+
+- (void)netServiceBrowser:(NSNetServiceBrowser *)netServiceBrowser didRemoveService:(NSNetService *)netService moreComing:(BOOL)moreServicesComing {
+    [_services removeObject:netService];
+    NSLog(@"didRemoveService: %@", netService);
 }
 
 @end
